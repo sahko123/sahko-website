b09a11d3bd6c24e6edf271db36af3207cc9ef982
sahko <sahko247@gmail.com>
2026-07-22 20:16:34 +0100

    Add Instagram to the Links section
    Generalized the icon rendering to a href-substring lookup table
    (youtube.com/instagram.com) instead of a YouTube-only ternary, so
    adding more platforms later is a one-line addition. Also scoped the
    icon-row layout to a new .social-links class rather than ul.plain,
    since that class is shared with the blog post list.

--- src/consts.ts
+++ src/consts.ts
@@ -8,6 +8,7 @@ export const NAV_LINKS = [
 
 export const SOCIAL_LINKS = [
 	{ href: 'https://www.youtube.com/@Sahko123', label: 'YouTube' },
+	{ href: 'https://www.instagram.com/sahko123/', label: 'Instagram' },
 	// { href: 'https://twitter.com/yourhandle', label: 'Twitter' },
 	// { href: 'https://twitch.tv/yourhandle', label: 'Twitch' },
 ];
